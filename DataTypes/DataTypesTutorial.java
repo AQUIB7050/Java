c094c89808ca32aeb1fbae909dd32350df12b342
AQUIB7050 <54367316+AQUIB7050@users.noreply.github.com>
2021-10-22 20:19:39 +0530

    Add files via upload

---
+++ DataTypes/DataTypesTutorial.java
@@ -0,0 +1,20 @@
+package dataTypes;
+
+public class DataTypesTutorial {
+
+	public static void main(String[] args) {
+		
+		byte x = 2;
+		int age = 10;
+		float rateOfInterest = 2.34f;
+		double rate = 1.26738;
+		
+		System.out.println(age);
+		
+		boolean isThisSeriesCool = true;
+		
+		char myCharacter = '@';
+
+	}
+
+}
